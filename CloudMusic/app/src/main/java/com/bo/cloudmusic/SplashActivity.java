a50a3299cf273ca433e86d93b7ad9293bc7225e8
lostmuchbit <1963498003@qq.com>
2022-04-16 13:24:21 +0800

    2022.4.16 13:23 编写启动界面

---
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.bo.cloudmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bo.cloudmusic.databinding.ActivityMainBinding;
+import com.bo.cloudmusic.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding activitySplashBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activitySplashBinding=ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(activitySplashBinding.getRoot());
+    }
+}
