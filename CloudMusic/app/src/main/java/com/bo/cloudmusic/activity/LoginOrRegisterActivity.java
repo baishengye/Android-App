2ea888681dff413f7b71394a5061ff55c2836606
lostmuchbit <1963498003@qq.com>
2022-04-17 19:56:24 +0800

    2022.4.17 19:55 黄油刀重构登录注册界面

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/LoginOrRegisterActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/LoginOrRegisterActivity.java
@@ -11,14 +11,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginOrRegisterActivity extends BaseCommonActivity implements View.OnClickListener {
-    @SuppressLint("NonConstantResourceId")
-    @BindView(R.id.bt_login)
-    Button bt_login;
-
-    @SuppressLint("NonConstantResourceId")
-    @BindView(R.id.bt_register)
-    Button bt_register;
+public class LoginOrRegisterActivity extends BaseCommonActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +28,6 @@ public class LoginOrRegisterActivity extends BaseCommonActivity implements View.
     protected void initListeners() {
         super.initListeners();
 
-        bt_register.setOnClickListener(this);
-    }
-
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            /*case R.id.bt_login:
-                startActivity(LoginActivity.class);
-                break;*/
-            case R.id.bt_register:
-                startActivity(RegisterActivity.class);
-                break;
-        }
     }
 
     /**
@@ -59,5 +37,14 @@ public class LoginOrRegisterActivity extends BaseCommonActivity implements View.
     @OnClick(R.id.bt_login)
     public void onLoginClick(){
         startActivity(LoginActivity.class);
+    }
+
+    /**
+     * 点击注册按钮
+     */
+    @SuppressLint("NonConstantResourceId")
+    @OnClick(R.id.bt_register)
+    public void onRegisterClick(){
+        startActivity(RegisterActivity.class);
     }
 }
