b5cf7fedb2af7b6474df78073090c66c1e916d12
lostmuchbit <1963498003@qq.com>
2022-04-17 22:37:52 +0800

    2022.4.17 22:37 给登录界面添加ToolBar

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/LoginActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/LoginActivity.java
@@ -1,19 +1,35 @@
 package com.bo.cloudmusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.bo.cloudmusic.R;
 
+import butterknife.BindView;
+
 /**
  * 登陆界面
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseCommonActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+
+        //初始化ToolBar
+        setSupportActionBar(toolbar);
+    }
+
+
 }
