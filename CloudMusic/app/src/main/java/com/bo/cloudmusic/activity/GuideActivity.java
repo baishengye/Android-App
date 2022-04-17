31193fae5a6101b610f8b4f576e023b1247a9283
lostmuchbit <1963498003@qq.com>
2022-04-17 14:05:11 +0800

    2022.4.17 14:04 给引导界面添加适配器，并且添加图片到适配器

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/GuideActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/GuideActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.viewpager.widget.ViewPager;
+
+import com.bo.cloudmusic.Adapter.GuideAdapter;
 import com.bo.cloudmusic.MainActivity;
 import com.bo.cloudmusic.R;
 import com.bo.cloudmusic.databinding.ActivityGuideBinding;
@@ -12,9 +15,14 @@ import com.bo.cloudmusic.fragment.GuideFragment;
 import com.bo.cloudmusic.utils.Constant;
 import com.bo.cloudmusic.utils.PreferencesUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GuideActivity extends BaseCommonActivity implements View.OnClickListener {
     private Button btLoginOrRegister;
     private Button btEnter;
+    private ViewPager vp;
+    private GuideAdapter adapter;
 
     @Override
     protected void initViews() {
@@ -22,14 +30,40 @@ public class GuideActivity extends BaseCommonActivity implements View.OnClickLis
 
         hideStatusBar();
 
+        //登录体验按钮
         btLoginOrRegister = findViewById(R.id.bt_login_or_register);
         btEnter = findViewById(R.id.bt_enter);
 
+        //ViewPager控件
+        vp=findViewById(R.id.vp);
+
         //测试显示GuideFragment
-        getSupportFragmentManager()
+        /*getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.container, GuideFragment.newInstance(R.drawable.guide1))
-                .commit();
+                .commit();*/
+    }
+
+    @Override
+    protected void initDatum() {
+        super.initDatum();
+
+        //创建适配器
+        adapter = new GuideAdapter(getSupportFragmentManager());
+
+        //设置适配器到控件
+        vp.setAdapter(adapter);
+
+        //准备数据
+        List<Integer> datum=new ArrayList<>();
+        datum.add(R.drawable.guide1);
+        datum.add(R.drawable.guide2);
+        datum.add(R.drawable.guide3);
+        datum.add(R.drawable.guide4);
+        datum.add(R.drawable.guide5);
+
+        //设置数据到适配器
+        adapter.setDatum(datum);
     }
 
     @Override
