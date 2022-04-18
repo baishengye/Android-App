ce4a083d33ef9b6da80cfddc20fe583245b7d1d8
lostmuchbit <1963498003@qq.com>
2022-04-18 15:16:05 +0800

    2022.4.18 15:15 正则表达式测试手机号

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/utils/StringUtil.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/utils/StringUtil.java
@@ -1,14 +1,15 @@
 package com.bo.cloudmusic.utils;
 
+import static com.bo.cloudmusic.utils.Constant.REGEX_PHONE;
+
 /**
  * 字符串相关⼯具类
  */
 public class StringUtil {
     /**
-     * 是否是⼿机号
+     * 是否符合⼿机号格式
      */
     public static boolean isPhone(String value) {
-        //TODO 实现⼿机号格式判断
-        return false;
+        return value.matches(REGEX_PHONE);
     }
 }
