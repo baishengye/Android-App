6dfdbf326c936687e04fd06be87b82b981b743f4
lostmuchbit <1963498003@qq.com>
2022-04-18 13:41:17 +0800

    2022.4.18 13:40 登录按钮响应

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/LoginActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/LoginActivity.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.bo.cloudmusic.R;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 登陆界面
  */
 public class LoginActivity extends BaseTitleActivity {
 
+    @BindView(R.id.et_username)
+    EditText et_username;
+
+    @BindView(R.id.et_password)
+    EditText et_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @OnClick(R.id.bt_login)
+    public void onLoginClick(Button view){
+        Toast.makeText(this,"点击登录",Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.bt_forget_password)
+    public void onForgetPasswordClick(Button view){
+        Toast.makeText(this,"点击忘记密码",Toast.LENGTH_SHORT).show();
     }
 }
