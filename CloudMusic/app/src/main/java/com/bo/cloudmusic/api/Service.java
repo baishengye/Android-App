1ba350c6745377f5feb2b2c33b85d6e380bc30cc
lostmuchbit <1963498003@qq.com>
2022-04-18 19:32:50 +0800

    2022.4.18 19:32 添加歌单的API

---
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/api/Service.java
@@ -0,0 +1,26 @@
+package com.bo.cloudmusic.api;
+
+import com.bo.cloudmusic.domain.SheetDetailWrapper;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * ⽹络接⼝配置
+ * <p>
+ * 之所以调⽤接⼝还能返回数据
+ * 是因为Retrofit框架内部处理了
+ * 这⾥不讲解原理
+ * 在《详解Retrofit》课程中讲解
+ */
+public interface Service {
+    /**
+     * 歌单详情
+     * @param id
+     * @return
+     */
+    @GET("v1/sheets/{id}")
+    Observable<SheetDetailWrapper> sheetDetail(@Path("id") String id);
+    //RxJava2中的Observable解析成SheetDetailWrapper
+}
