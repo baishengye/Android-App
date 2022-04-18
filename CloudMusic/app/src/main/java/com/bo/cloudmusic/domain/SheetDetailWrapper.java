77edd4c21ced5dedfc09ba248ac83030aae44114
lostmuchbit <1963498003@qq.com>
2022-04-18 19:19:00 +0800

    2022.4.18 19:18 添加歌单的数据模型类

---
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/domain/SheetDetailWrapper.java
@@ -0,0 +1,20 @@
+package com.bo.cloudmusic.domain;
+
+/**
+ * 歌单详情包裹对象
+ * 仅用于测试
+ */
+public class SheetDetailWrapper {
+    /**
+     * 歌单对象
+     */
+    private Sheet data;
+
+    public Sheet getData() {
+        return data;
+    }
+
+    public void setData(Sheet data) {
+        this.data = data;
+    }
+}
