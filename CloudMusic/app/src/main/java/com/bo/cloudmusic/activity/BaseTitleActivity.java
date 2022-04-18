976cbb5be711b6a1396e23c252e43f9d578e3025
lostmuchbit <1963498003@qq.com>
2022-04-18 08:29:43 +0800

    2022.4.18 8:29  添加Toolbar中的返回按钮

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/BaseTitleActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/BaseTitleActivity.java
@@ -1,5 +1,8 @@
 package com.bo.cloudmusic.activity;
 
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 
 import com.bo.cloudmusic.R;
@@ -17,6 +20,39 @@ public class BaseTitleActivity extends BaseCommonActivity{
 
         //初始化ToolBar
         setSupportActionBar(toolbar);
+
+        //是否显示返回按钮
+        if(isShowBackMenu()){
+            ShowBackMenu();
+        }
     }
 
+    /**
+     * 显示返回按钮
+     */
+    private void ShowBackMenu() {
+        //显示返回按钮
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * 是否显示返回按钮
+     */
+    protected boolean isShowBackMenu() {
+        return true;
+    }
+
+    /**
+     * 菜单点击了返回按钮
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            //Toolbar返回按钮点击
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
