d365313bc6a075a38927fb66b181772450db9d54
lostmuchbit <1963498003@qq.com>
2022-05-01 10:12:12 +0800

    ：2022.5.1  10:11 让指示器和viewPage配合工作

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/Adapter/MainAdapter.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/Adapter/MainAdapter.java
@@ -1,18 +1,23 @@
 package com.bo.cloudmusic.Adapter;
 
+import static com.mob.tools.utils.Strings.getString;
+
 import android.content.Context;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.bo.cloudmusic.R;
 import com.bo.cloudmusic.fragment.DiscoveryFragment;
 import com.bo.cloudmusic.fragment.FeedFragment;
 import com.bo.cloudmusic.fragment.MeFragment;
 import com.bo.cloudmusic.fragment.VideoFragment;
 
 public class MainAdapter extends BaseFragmentPagerAdapter<Integer> {
-
+    //指示器标题
+    private static final Integer[] titleResources={R.string.me,R.string.discovery,R.string.friend,R.string.video};
 
     /**
      * 构造方法
@@ -43,5 +48,9 @@ public class MainAdapter extends BaseFragmentPagerAdapter<Integer> {
         }
     }
 
-
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return context.getString(titleResources[position]);
+    }
 }
--- CloudMusic/app/src/main/java/com/bo/cloudmusic/MainActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/MainActivity.java
@@ -1,33 +1,38 @@
 package com.bo.cloudmusic;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.viewpager.widget.ViewPager;
-
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+
 import com.bo.cloudmusic.Adapter.MainAdapter;
-import com.bo.cloudmusic.activity.BaseCommonActivity;
 import com.bo.cloudmusic.activity.BaseTitleActivity;
 import com.bo.cloudmusic.activity.SettingActivity;
 import com.bo.cloudmusic.activity.WebViewActivity;
 import com.bo.cloudmusic.api.Api;
-import com.bo.cloudmusic.databinding.ActivityMainBinding;
 import com.bo.cloudmusic.domain.User;
 import com.bo.cloudmusic.domain.response.DetailResponse;
 import com.bo.cloudmusic.listener.HttpObserver;
 import com.bo.cloudmusic.utils.Constant;
 import com.bo.cloudmusic.utils.ImageUtil;
 import com.bo.cloudmusic.utils.LogUtil;
-import com.bo.cloudmusic.utils.ToastUtil;
-import com.bumptech.glide.Glide;
+
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +52,10 @@ public class MainActivity extends BaseTitleActivity {
     @BindView(R.id.vp)
     ViewPager vp;
 
+    /*指示器*/
+    @BindView(R.id.mi)
+    MagicIndicator mi;
+
     /*昵称*/
     @BindView(R.id.tv_nickname)
     TextView tv_nickname;
@@ -58,6 +67,9 @@ public class MainActivity extends BaseTitleActivity {
     /*头像*/
     @BindView(R.id.iv_avatar)
     ImageView iv_avatar;
+
+    //适配器
+    private MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,11 +94,6 @@ public class MainActivity extends BaseTitleActivity {
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
 
-                //获取⽤户信息
-                //当然可以在⽤户要显示侧滑的时候
-                //才获取⽤户信息
-                //这样可以减少请求
-                fetchData();
             }
         };
 
@@ -97,7 +104,7 @@ public class MainActivity extends BaseTitleActivity {
         actionBarDrawerToggle.syncState();
 
         //创建adapter
-        MainAdapter adapter = new MainAdapter(getMainActivity(), getSupportFragmentManager());
+        adapter = new MainAdapter(getMainActivity(), getSupportFragmentManager());
         //把适配器设置到vp中
         vp.setAdapter(adapter);
 
@@ -112,11 +119,98 @@ public class MainActivity extends BaseTitleActivity {
         datum.add(2);
         datum.add(3);
         adapter.setDatum(datum);
+
+        //创建通用的指示器
+        CommonNavigator commonNavigator = new CommonNavigator(getMainActivity());
+
+        //设置适配器
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            /**
+             * 指示器数量
+             * @return
+             */
+            @Override
+            public int getCount() {
+                return datum.size();
+            }
+
+            /**
+             * 返回当前位置的标题
+             * @param context
+             * @param index
+             * @return
+             */
+            @Override
+            public IPagerTitleView getTitleView(Context context, int index) {
+                //创建简单的文本控件
+                SimplePagerTitleView titleView = new SimplePagerTitleView(context);
+
+                //默认颜色
+                titleView.setNormalColor(getResources().getColor(R.color.table_normal));
+
+                //选中后的颜色
+                titleView.setSelectedColor(getResources().getColor(R.color.white));
+
+                //显示的文本
+                titleView.setText(adapter.getPageTitle(index));
+
+                //设置点击回调监听
+                titleView.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        //viewpager跳转到index这个位置
+                        vp.setCurrentItem(index);
+                    }
+                });
+
+                return titleView;
+            };
+
+            /**
+             * 返回指示器
+             * 就是下面的线
+             * @param context
+             * @return
+             */
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                //创建一条线
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+
+                //线的宽度和内容宽度一样
+                indicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+
+                //高亮颜色
+                indicator.setColors(Color.WHITE);
+
+                return indicator;
+            }
+        });
+
+        //如果位置显示不下指示器的时候
+        //是否自动调整
+        commonNavigator.setAdjustMode(true);
+
+        //设置到导航器
+        mi.setNavigator(commonNavigator);
+
+        //让指示器和ViewPager配合工作哟
+        ViewPagerHelper.bind(mi,vp);
     }
 
     @Override
     protected void initDatum() {
         super.initDatum();
+
+        //默认选中第二个界面
+        vp.setCurrentItem(1);
+
+        //获取⽤户信息
+        //当然可以在⽤户要显示侧滑的时候
+        //才获取⽤户信息
+        //这样可以减少请求
+        fetchData();
 
         //用Glide显示本地资源目录的图片
         /*Glide.with(this)
