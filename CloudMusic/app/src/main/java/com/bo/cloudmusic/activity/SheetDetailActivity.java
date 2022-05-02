f5b931261e2b13f5a82332d6853d5d04074ec0ba
lostmuchbit <1963498003@qq.com>
2022-05-02 20:50:05 +0800

    ：2022.5.2 20:49 完成歌单详情的头部布局

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/SheetDetailActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/SheetDetailActivity.java
@@ -10,6 +10,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bo.cloudmusic.Adapter.SongAdapter;
 import com.bo.cloudmusic.R;
@@ -20,7 +24,10 @@ import com.bo.cloudmusic.domain.response.DetailResponse;
 import com.bo.cloudmusic.domain.response.ListResponse;
 import com.bo.cloudmusic.listener.HttpObserver;
 import com.bo.cloudmusic.utils.Constant;
+import com.bo.cloudmusic.utils.ImageUtil;
 import com.bo.cloudmusic.utils.LogUtil;
+
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -49,6 +56,47 @@ public class SheetDetailActivity extends BaseTitleActivity {
 
     @BindView(R.id.rv)
     RecyclerView rv;
+
+    /**
+     * 头部容器
+     */
+    private LinearLayout ll_header;
+    /**
+     * 封面banner
+     */
+    private ImageView iv_banner;
+    /**
+     * 标题
+     */
+    private TextView tv_title;
+    /**
+     * 头像
+     */
+    private ImageView iv_avatar;
+    /**
+     *按钮容器
+     */
+    private LinearLayout ll_comment_container;
+    /**
+     * 昵称
+     */
+    private TextView tv_nickname;
+    /**
+     * 评论数
+     */
+    private TextView tv_comment_count;
+    /**
+     * 收藏按钮
+     */
+    private Button bt_collection;
+    /**
+     * 水平容器
+     */
+    private LinearLayout ll_play_all_container;
+    /**
+     * 歌曲数量
+     */
+    private TextView tv_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,6 +150,37 @@ public class SheetDetailActivity extends BaseTitleActivity {
         //从xml填充布局
         View view=getLayoutInflater().inflate(R.layout.header_sheet_detail, (ViewGroup) rv.getParent(),false);
 
+        //头部容器
+        ll_header = view.findViewById(R.id.ll_header);
+        
+        //封⾯图
+        iv_banner = view.findViewById(R.id.iv_banner);
+        
+        //标题
+        tv_title = view.findViewById(R.id.tv_title);
+
+        //歌单创建者头像
+        iv_avatar = view.findViewById(R.id.iv_avatar);
+
+        //歌单创建者昵称
+        tv_nickname = view.findViewById(R.id.tv_nickname);
+
+        //评论容器
+        ll_comment_container = view.findViewById(R.id.ll_comment_container);
+
+        //评论数
+        tv_comment_count = view.findViewById(R.id.tv_comment_count);
+
+        //收藏按钮
+        bt_collection = view.findViewById(R.id.bt_collection);
+
+        //播放全部容器
+        ll_play_all_container = view.findViewById(R.id.ll_play_all_container);
+
+        //歌曲数
+        tv_count = view.findViewById(R.id.tv_count);
+        
+
         //返回view
         return view;
     }
@@ -128,6 +207,30 @@ public class SheetDetailActivity extends BaseTitleActivity {
         if(songs!=null&&songs.size()>0){
             adapter.replaceData(songs);
         }
+
+        //显示封面
+        if(StringUtils.isBlank(data.getBanner())){
+            //空的就显示默认图片
+            iv_banner.setImageResource(R.drawable.placeholder);
+        }else{
+            //有图片
+            ImageUtil.show(getMainActivity(),iv_banner,data.getBanner());
+        }
+
+        //显示标题
+        tv_title.setText(data.getTitle());
+
+        //头像
+        ImageUtil.showAvatar(getMainActivity(),iv_avatar,data.getUser().getAvatar());
+
+        //昵称
+        tv_nickname.setText(data.getUser().getNickname());
+
+        //评论数
+        tv_comment_count.setText(String.valueOf(data.getComments_count()));
+
+        //音乐数
+        tv_count.setText(String.valueOf(data.getSongs_count()));
 
     }
 
