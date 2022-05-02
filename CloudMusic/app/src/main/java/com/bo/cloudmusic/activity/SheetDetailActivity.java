58666aab1666729a82ba3fb58809038f3ec6857f
lostmuchbit <1963498003@qq.com>
2022-05-02 15:20:48 +0800

    ：2022.5.2 15:20 请求歌单详情的数据

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/SheetDetailActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/SheetDetailActivity.java
@@ -3,16 +3,34 @@ package com.bo.cloudmusic.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.bo.cloudmusic.R;
+import com.bo.cloudmusic.api.Api;
+import com.bo.cloudmusic.domain.Sheet;
+import com.bo.cloudmusic.domain.response.DetailResponse;
+import com.bo.cloudmusic.domain.response.ListResponse;
+import com.bo.cloudmusic.listener.HttpObserver;
 import com.bo.cloudmusic.utils.Constant;
+import com.bo.cloudmusic.utils.LogUtil;
+
+import java.util.List;
 
 /**
  * 歌单详情界面
  */
 public class SheetDetailActivity extends BaseTitleActivity {
 
+    private static final String TAG = "SheetDetailActivity";
+    /**
+     * 歌单id
+     */
     private String id;
+
+    /**
+     * 歌单数据
+     */
+    private Sheet data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +44,26 @@ public class SheetDetailActivity extends BaseTitleActivity {
 
         //获取传递的参数
         id = extraId();
+
+        fetchData();
+    }
+
+    /**
+     * 请求歌单数据
+     */
+    private void fetchData(){
+        Api.getInstance().sheetDetail(id)
+                .subscribe(new HttpObserver<DetailResponse<Sheet>>() {
+                    @Override
+                    public void onSucceeded(DetailResponse<Sheet> data) {
+                        next(data.getData());
+                    }
+                });
+    }
+
+    private void next(Sheet data) {
+        this.data=data;
+        LogUtil.d(TAG,data.toString());
     }
 
     /**
