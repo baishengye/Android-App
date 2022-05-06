1569d56e7b9ab87e76888379ed77a17ebffe9d61
lostmuchbit <1963498003@qq.com>
2022-05-06 08:30:50 +0800

    2022 5.6 8.30  添加跳转到评论界面

---
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/CommentActivity.java
@@ -0,0 +1,16 @@
+package com.bo.cloudmusic.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bo.cloudmusic.R;
+
+public class CommentActivity extends BaseTitleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
