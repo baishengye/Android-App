2ed9aecc576b7a608cd8b7698913a0ba1ffc272e
lostmuchbit <1963498003@qq.com>
2022-05-07 09:42:06 +0800

    3774  添加音乐播放管理器的接口

---
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/manager/MusicPlayerManager.java
@@ -0,0 +1,7 @@
+package com.bo.cloudmusic.manager;
+
+/**
+ * 音乐播放器对外暴露的接口
+ */
+public interface MusicPlayerManager {
+}
---
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/manager/impl/MusicPlayerManagerImpl.java
@@ -0,0 +1,9 @@
+package com.bo.cloudmusic.manager.impl;
+
+import com.bo.cloudmusic.manager.MusicPlayerManager;
+
+/**
+ * 音乐播放器的具体实现
+ */
+public class MusicPlayerManagerImpl implements MusicPlayerManager {
+}
