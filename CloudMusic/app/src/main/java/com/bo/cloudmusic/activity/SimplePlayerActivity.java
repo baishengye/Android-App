6626f2464dc20ea8e1a25ca66b7356eb23982453
lostmuchbit <1963498003@qq.com>
2022-05-07 09:34:34 +0800

    3773 监听进度条

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/SimplePlayerActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/SimplePlayerActivity.java
@@ -1,12 +1,20 @@
 package com.bo.cloudmusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.bo.cloudmusic.R;
+import com.bo.cloudmusic.utils.LogUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 简单的播放器实现
@@ -17,12 +25,65 @@ import com.bo.cloudmusic.R;
  * 从而把播放器相关逻辑实现完成
  * 然后在对接的黑胶唱片，就相对来说简单一点
  */
-public class SimplePlayerActivity extends BaseTitleActivity {
+public class SimplePlayerActivity extends BaseTitleActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private static final String TAG = "SimplePlayerActivity";
+    /**
+     * 列表
+     */
+    @BindView(R.id.rv)
+    RecyclerView rv;
+
+    /**
+     * 标题
+     */
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+
+    /**
+     * 开始时间
+     */
+    @BindView(R.id.tv_start)
+    TextView tv_start;
+
+    /**
+     * 拖拽进度控件
+     */
+    @BindView(R.id.sb_progress)
+    SeekBar sb_progress;
+
+    /**
+     * 结束时间
+     */
+    @BindView(R.id.tv_end)
+    TextView tv_end;
+
+    /**
+     * 播放按钮
+     */
+    @BindView(R.id.bt_play)
+    Button bt_play;
+
+    /**
+     * 循环模式
+     */
+    @BindView(R.id.bt_loop_model)
+    Button bt_loop_model;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_player);
+    }
+
+
+    @Override
+    protected void initListeners() {
+        super.initListeners();
+
+        //拖拽进度条控件
+        sb_progress.setOnSeekBarChangeListener(this);
     }
 
     /**
@@ -33,5 +94,59 @@ public class SimplePlayerActivity extends BaseTitleActivity {
         Intent intent=new Intent(activity,SimplePlayerActivity.class);
 
         activity.startActivity(intent);
+    }
+
+    /**
+     * 上一曲按钮
+     */
+    @OnClick(R.id.bt_previous)
+    public void onPreviousClick(){
+        LogUtil.d(TAG,"bt_previous");
+    }
+
+    /**
+     * 播放按钮
+     */
+    @OnClick(R.id.bt_play)
+    public void onPlayClick(){
+        LogUtil.d(TAG,"onPlayClick");
+    }
+
+    /**
+     * 下一曲按钮
+     */
+    @OnClick(R.id.bt_next)
+    public void onNextClick(){
+        LogUtil.d(TAG,"bt_next");
+    }
+
+
+    /**
+     * 进度改变时
+     * @param seekBar  进度条
+     * @param progress  改变后的进度
+     * @param fromUser  是否是用户触发的
+     */
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        LogUtil.d(TAG,"onProgressChanged");
+    }
+
+    /**
+     * 开始拖拽进度条
+     * @param seekBar
+     */
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        LogUtil.d(TAG,"onStartTrackingTouch");
+    }
+
+    /**
+     * 停止拖拽进度条
+     * @param seekBar
+     */
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        LogUtil.d(TAG,"onStopTrackingTouch");
     }
 }
