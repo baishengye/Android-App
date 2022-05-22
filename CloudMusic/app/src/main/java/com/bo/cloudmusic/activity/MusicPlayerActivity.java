ea8ab3d57c58be7f1da07e091283709d8bd711b6
lostmuchbit <1963498003@qq.com>
2022-05-22 09:19:09 +0800

    69_6 黑胶唱片背景⾼斯模糊

--- CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/MusicPlayerActivity.java
+++ CloudMusic/app/src/main/java/com/bo/cloudmusic/activity/MusicPlayerActivity.java
@@ -1,9 +1,14 @@
 package com.bo.cloudmusic.activity;
 
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -13,8 +18,17 @@ import com.bo.cloudmusic.manager.ListManager;
 import com.bo.cloudmusic.manager.MusicPlayerManager;
 import com.bo.cloudmusic.service.MusicPlayerService;
 import com.bo.cloudmusic.utils.ImageUtil;
+import com.bo.cloudmusic.utils.ResourceUtil;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import org.apache.commons.lang3.StringUtils;
 
 import butterknife.BindView;
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class MusicPlayerActivity extends BaseTitleActivity {
 
@@ -79,7 +93,48 @@ public class MusicPlayerActivity extends BaseTitleActivity {
         toolbar.setSubtitle(data.getSinger().getNickname());
 
         //显示背景
-        ImageUtil.show(getMainActivity(),iv_background,data.getBanner());
+        //ImageUtil.show(getMainActivity(),iv_background,data.getBanner());
+        //高斯模糊的背景
+        RequestBuilder<Drawable> requestBuilder = Glide.with(this).asDrawable();
+
+        if(StringUtils.isBlank(data.getBanner())){
+            //没有封面图
+            //使用默认图片
+            requestBuilder.load(R.drawable.default_album);
+        }else{
+            //使用真实图片
+            requestBuilder.load(ResourceUtil.resourceUri(data.getBanner()));
+        }
+
+        //创建请求选项
+        //传⼊了BlurTransformation
+        //⽤来实现⾼斯模糊
+        //radius:模糊半径；值越⼤越模糊
+        //sampling:采样率；值越⼤越模糊
+        RequestOptions options = bitmapTransform(new BlurTransformation(25, 3));
+
+        //加载图片
+        requestBuilder.apply(options)
+                .into(new CustomTarget<Drawable>() {
+                    /**
+                     * 资源下载成功
+                     * @param resource
+                     * @param transition
+                     */
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        //设置到背景图片上
+                        iv_background.setImageDrawable(resource);
+
+                        //创建切换动画工具类
+                        //new SwitchDrawableUtil(iv_background.getDrawable(),resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
     }
 
     /**
